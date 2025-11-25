d1cfc4de15d84a807b4a6a6f49edb7f56e318392
GL0riFy <siyyhoi@gmail.com>
2025-11-25 12:22:13 +0700

    asdpkaspodsad

--- index.js
+++ index.js
@@ -10,15 +10,12 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const pool = mysql2.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 });
 
 function authMiddleware(req, res, next) {
