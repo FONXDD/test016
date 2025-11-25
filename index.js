3477415f7efb946862cf16a4d6fe04745da1edc8
GL0riFy <siyyhoi@gmail.com>
2025-11-25 12:25:28 +0700

    sadasdasdasd

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const mysql2 = require("mysql2/promise");
+const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 app.use(cors());
